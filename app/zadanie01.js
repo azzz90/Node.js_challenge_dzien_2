ae61516f9ed1379b1d96017ccbfb4c4c4b545fe7
azzz90 <aleksander.ziolkowski90@gmail.com>
2018-01-30 22:39:48 +0100

    zadanie 1

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,21 @@
 //Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) =>{
+  if (err === null){
+    console.log('śmiga', data);
+    let dataParsed = JSON.parse(data);
+    let result = dataParsed.reduce((a,b) => a+b,0)
+    //console.log(result);
+
+    fs.writeFile('./data/zadanie01/sum.txt', result, err => {
+      if (err === null){
+        console.log('zapisano',result);
+      } else {
+        console.log('zapis nieudany', err);
+      }
+    })
+  } else {
+    console.log('nie śmiga', err);
+  }
+})
